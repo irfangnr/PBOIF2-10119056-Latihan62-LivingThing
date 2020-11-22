226bc490efb69c257ec551faebccadb22c6b3721
ACER <ACER@Irfan>
2020-11-22 19:45:16 +0700

    upload project

---
+++ src/pboif2/pkg10119056/latihan62/livingthing/PBOIF210119056Latihan62LivingThing.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119056.latihan62.livingthing;
+
+/**
+ *
+ * @author ACER
+ */
+public class PBOIF210119056Latihan62LivingThing {
+
+    public static void main(String[] args) {
+        Human human=new Human();
+        human.setNama("Rizki Adam Kurniawan");
+        human.walk(human.getNama());
+    }
+    
+}
